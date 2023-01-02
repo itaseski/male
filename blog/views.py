18da8d80e4d475d9033a00d857991304ec8e379c
itaseski <itaseski@t.mk>
2023-01-02 13:40:46 +0100

    Handling pagination errors

--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,20 @@ from .models import Post
 
 def post_list(request):
     post_list = Post.published.all()
-    # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    page_number = request.GET.get('page', 1) # If the page parameter is not in the GET parameters of the request, we use the default value 1 to load the first page of results    
 
-    context = {'posts':posts}
+    try:
+        posts_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts_obj = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts_obj = paginator.page(paginator.num_pages)
+
+    context = {'posts':posts_obj}
+
     return render(request, 'blog/post/list.html', context)
 
 def post_detail(request, year, month, day, post):
